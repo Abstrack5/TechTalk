25211c3226245564ed68e84a2d18bff9874d33de
Armon Ahmadi <abstrack@Armons-MacBook-Air.local>
2022-06-11 01:27:16 -0400

    added second post route get for id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const sequelize = require("../../config/connection");
 const { Post, User, Comment } = require("../../models");
 
-
+// 
 router.get("/", (req, res) => {
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "title", "post_url", "created_at"],
     include: [
       {
         model: User,
@@ -18,6 +18,45 @@ router.get("/", (req, res) => {
       console.error(err);
       res.status(500).json(err);
   })
-})
+});
+
+// 
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ["id", "title", "post_url", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"]
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include:
+          {
+            model: User,
+            attributes: ["username"]
+          }
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post data found'});
+      return;
+    } else {
+      res.json(dbPostData);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+// findOne, Update, Delete, Create
 
 module.exports = router;
