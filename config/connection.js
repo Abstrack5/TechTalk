fc4573fb4bdadb29509007407bafa2ab7568def7
Abstrack <Armon_ah05@outlook.com>
2022-06-14 19:40:10 -0400

    heroku giving internal server error

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,6 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
-// // creates connection to DB
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: "localhost",
-//     dialect: "mysql",
-//     port: 3306,
-//   }
-// );
 
 let sequelize;
 
@@ -30,5 +19,17 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
+
+// // creates connection to DB
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
 module.exports = sequelize;
