adcb378724a39bd99a883b7b9487b37f0a683f38
Abstrack <Armon_ah05@outlook.com>
2022-06-14 19:50:54 -0400

    added jawsdb connection, heroku create, heroku pushed

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
     {
       host: "localhost",
       dialect: "mysql",
